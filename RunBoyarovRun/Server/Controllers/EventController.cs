bd049068e50e432da3743fe3d1b41d898143bd1e
TonyStamatova <a.stamatova@abv.bg>
2021-05-24 16:24:38 +0300

    Add Events page

---
+++ RunBoyarovRun/Server/Controllers/EventController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RunBoyarovRun.Shared;
+using System;
+using System.Collections.Generic;
+
+namespace RunBoyarovRun.Server.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class EventController : ControllerBase
+    {
+        private readonly Event[] Events = new Event[]
+        {
+            new Event
+            {
+                Date = DateTime.UtcNow,
+                Title = "First Event",
+                Description = "Some detailed description here",
+                Image = "https://upload.wikimedia.org/wikipedia/commons/1/1c/Beijing_2008%2C_Beijing_National_Stadium.jpg"
+            },
+            new Event
+            {
+                Date = DateTime.UtcNow.AddDays(2),
+                Title = "Second Event",
+                Description = "Some detailed description here",
+                Image = "https://upload.wikimedia.org/wikipedia/commons/1/1c/Beijing_2008%2C_Beijing_National_Stadium.jpg"
+            }
+        };
+
+        [HttpGet]
+        public IEnumerable<Event> Get()
+        {
+            return this.Events;
+        }
+    }
+}
---
+++ RunBoyarovRun/Shared/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RunBoyarovRun.Shared
+{
+    public class Event
+    {
+        public DateTime Date { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+    }
+}
